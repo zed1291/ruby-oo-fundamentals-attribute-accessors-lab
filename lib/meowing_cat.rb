36f98a3c0e2e50a50c67aebce14b3858e09c3946
“zed” <zed@kolk.me>
2021-01-13 19:42:53 -0600

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,20 @@
-## code your solution here. 
+class Cat
+    def initialize
+        @name="Maru"
+    end
+
+    attr_accessor :cat
+
+    def meow
+        puts "meow!"
+    end
+
+    def name
+        @name
+    end
+
+    def name=name
+        @name
+    end
+    
+end
